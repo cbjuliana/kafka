e76ac833b9126addb66916034b67dd79546b3797
Juliana <cbatista.juliana@gmail.com>
2021-08-21 09:56:32 -0300

    Adicionando EmailService e LogService

--- ecommerce/src/main/java/com/example/kafka/ecommerce/NewOrderMain.java
+++ ecommerce/src/main/java/com/example/kafka/ecommerce/NewOrderMain.java
@@ -3,6 +3,7 @@ package com.example.kafka.ecommerce;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -13,17 +14,24 @@ public class NewOrderMain {
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		
 		var producer = new KafkaProducer<String, String>(properties());
-		var value = "132123,67523,7894589745";
-		var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-		producer.send(record, (data, ex) -> {
+		
+		Callback callback = (data, ex) -> {
 			if (ex != null) {
 				ex.printStackTrace();
 				return;
 			}
 			System.out.println("Sucesso enviando " + data.topic() + " :::partition " + data.partition() + " / offset " + data.offset() + "/timestamp " + data.timestamp());
-		}).get();		
+		};
+		
+		var order = "132123,67523,7894589745";
+		var orderRecord = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", order, order);		
+		producer.send(orderRecord, callback).get();	
+		
+		var email = "Thak you for your order. We are processing your order.";
+		var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);		
+		producer.send(emailRecord, callback).get();		
 	}
-
+	
 	private static Properties properties() {
 		var properties = new Properties();
 		properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
